c53784835fcc988f7b5693f8ddfd430c757b1d39
farhan <www.mfarvirus@gmail.com>
2022-02-08 20:03:54 +0500

    Add project files.

---
+++ ytdownloader/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace ytdownloader
+{
+    partial class F_Downloader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(F_Downloader));
+            this.link_box = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.audioonly = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // link_box
+            // 
+            this.link_box.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.link_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.link_box.Location = new System.Drawing.Point(187, 357);
+            this.link_box.Multiline = true;
+            this.link_box.Name = "link_box";
+            this.link_box.Size = new System.Drawing.Size(449, 32);
+            this.link_box.TabIndex = 0;
+            this.link_box.Text = "Youtube Url";
+            this.link_box.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(86)))), ((int)(((byte)(175)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(404, 422);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(294, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // audioonly
+            // 
+            this.audioonly.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.audioonly.AutoSize = true;
+            this.audioonly.BackColor = System.Drawing.Color.Black;
+            this.audioonly.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.audioonly.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.audioonly.Location = new System.Drawing.Point(780, 359);
+            this.audioonly.Name = "audioonly";
+            this.audioonly.Size = new System.Drawing.Size(123, 28);
+            this.audioonly.TabIndex = 3;
+            this.audioonly.Text = "Audio Only";
+            this.audioonly.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(404, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(0, 0);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(417, 88);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(261, 254);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1080p",
+            "720p",
+            "480p"});
+            this.comboBox1.Location = new System.Drawing.Point(643, 357);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 32);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // F_Downloader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1133, 653);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.audioonly);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.link_box);
+            this.ForeColor = System.Drawing.Color.Cornsilk;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "F_Downloader";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "F-Yt-Downloader";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox link_box;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox audioonly;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ ytdownloader/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+using YoutubeExplode.Converter;
+using System.IO;
+
+namespace ytdownloader
+{
+    public partial class F_Downloader : Form
+    {
+        public F_Downloader()
+        {
+            InitializeComponent();
+        }
+       
+        private async  void button1_Click(object sender, EventArgs e)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
+            Console.WriteLine(path);
+
+            string link = link_box.Text ;
+            string reg = @"^https://\w{0,3}.?youtube+\.\w{2,3}/watch\?v=[\w-]{11}";
+            bool x = System.Text.RegularExpressions.Regex.IsMatch(link, reg);
+            if (!x)
+            {
+                MessageBox.Show("Enter a valid url");
+                return;
+            }
+            if(audioonly.Checked)
+            {
+                button1.Enabled = false;
+                button1.Text = "Downloading...";
+                var yt = new YoutubeClient();
+                var vid = await yt.Videos.GetAsync(link);
+                var tit = vid.Title;
+                string invalid = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+
+                foreach (char c in invalid)
+                {
+                    tit = tit.Replace(c.ToString(), "");
+                }
+                var auth = vid.Author.Title;
+                var dur = vid.Duration;
+                Console.WriteLine(tit);
+                Console.WriteLine(auth);
+                Console.WriteLine(dur);
+                var sM = await yt.Videos.Streams.GetManifestAsync(link);
+                var sI = sM.GetAudioOnlyStreams().GetWithHighestBitrate();
+
+                await yt.Videos.Streams.DownloadAsync(sI, path + @"\" + $"{tit}audio.{sI.Container}");
+                MessageBox.Show("Audio Downloaded", "Message");
+                button1.Text = "Download";
+
+                button1.Enabled = true;
+                link_box.Text = "";
+                audioonly.Checked = false;
+                return;
+
+
+
+            }
+            else
+            {
+                Console.WriteLine(comboBox1.Text);
+                if(comboBox1.Text == "")
+                {
+                    MessageBox.Show("Resolution is required");
+                    return;
+
+                }
+                button1.Enabled = false;
+                button1.Text = "Downloading...";
+
+                var youtube = new YoutubeClient();
+                var video = await youtube.Videos.GetAsync(link);
+                var title = video.Title;
+                string invalid = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+
+                foreach (char c in invalid)
+                {
+                    title = title.Replace(c.ToString(), "");
+                }
+                var author = video.Author.Title;
+                var duration = video.Duration;
+                Console.WriteLine(title);
+                Console.WriteLine(author);
+                Console.WriteLine(duration);
+                var streamManifest = await youtube.Videos.Streams.GetManifestAsync(link);
+                var audioStreamInfo = streamManifest.GetAudioStreams().GetWithHighestBitrate();
+                try
+                {
+                    var videoStreamInfo = streamManifest.GetVideoStreams().First(s => s.VideoQuality.Label == comboBox1.Text);
+                    var streamInfos = new IStreamInfo[] { audioStreamInfo, videoStreamInfo };
+                    await youtube.Videos.DownloadAsync(streamInfos, new ConversionRequestBuilder(path + @"\" + $"{title}.mp4").Build());
+                    MessageBox.Show("Video Downloaded", "Message");
+                    button1.Enabled = true;
+                    link_box.Text = "";
+
+
+                }
+                catch (Exception ex)
+                {
+                    var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+                    await youtube.Videos.Streams.DownloadAsync(streamInfo, path + @"\" + $"{title}.{streamInfo.Container}");
+                    MessageBox.Show("FFmpeg missing so Downloaded in highest quality available. Please add ffmpeg to path", "Message");
+                    MessageBox.Show("Video Downloaded", "Message");
+                    button1.Enabled = true;
+                    link_box.Text = "";
+                }
+                button1.Enabled = true;
+                button1.Text = "Download";
+                return;
+
+            }
+         
+
+
+
+
+        }
+
+     
+    }
+}
